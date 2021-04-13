d887fc63ebe776e2aa2e12fdb55aae2e09d7c319
lxj <1073830130@qq.com>
2021-04-13 14:22:56 +0800

    publish: 1.1.2 -> 更新依赖

--- src/demo/play.tsx
+++ src/demo/play.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import Wine from '../index';
 import '../style.css';
 
 const Play = () => {
+  useEffect(() => {
+    Wine.events.change.on(() => {
+      console.log(111);
+    });
+  }, []);
+
   function renderHandle() {
     Wine.render({
       content: (
